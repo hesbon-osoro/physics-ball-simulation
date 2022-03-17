c98dbe8bcbe86c1c7c23af16c961e57ab7d99b6a
wazimuhb <hesbonosoro1@gmail.com>
2022-03-17 10:02:09 +0300

    react chart

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { CartesianGrid, Line, LineChart, Tooltip, XAxis } from 'recharts';
 export const Chart = ({ data }) => {
-  return (
-    <LineChart width={400} height={400} data={data}>
-      <XAxis dataKey="name" />
-      <Tooltip />
-      <CartesianGrid stroke="#f5f5f5" />
-      <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-      <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
-    </LineChart>
-  );
+	return (
+		<LineChart width={400} height={400} data={data}>
+			<XAxis dataKey="name" />
+			<Tooltip />
+			<CartesianGrid stroke="#f5f5f5" />
+			<Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+			<Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+		</LineChart>
+	);
 };
